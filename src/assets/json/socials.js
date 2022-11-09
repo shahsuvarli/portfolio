02c5c38974a76d20ce5cf5eb6376056f6572ea15
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-11-10 00:19:10 +0400

    deployed to shahsuvarli.com

--- src/assets/json/socials.js
+++ src/assets/json/socials.js
@@ -2,30 +2,32 @@ import { BsGithub, BsTwitter } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 
+const size = 50;
+const color = "beige";
 
 export const socials = [
   {
     id: 0,
     name: "Linkedin",
-    img: <FaLinkedinIn size={50} color="beige" />,
+    img: <FaLinkedinIn size={size} color={color} />,
     link: "https://www.linkedin.com/in/shahsuvarli/",
   },
   {
     id: 1,
     name: "Github",
-    img: <BsGithub size={50} color="beige" />,
+    img: <BsGithub size={size} color={color} />,
     link: "http://github.com/shahsuvarli",
   },
   {
     id: 2,
     name: "Instagram",
-    img: <FiInstagram size={50} color="beige" />,
+    img: <FiInstagram size={size} color={color} />,
     link: "https://www.instagram.com/shahsuvarli_/",
   },
   {
     id: 3,
     name: "Twitter",
-    img: <BsTwitter size={50} color="beige" />,
+    img: <BsTwitter size={size} color={color} />,
     link: "https://twitter.com/EShahsuvarli",
   },
 ];
