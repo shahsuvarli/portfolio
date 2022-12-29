63ab6a765dcc17851f0674d9c16fa203cfe0492f
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-12-30 03:22:03 +0400

    animation added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,10 +5,15 @@ import { socials } from "../assets/json/socials";
 import SocialIcon from "./SocialIcon";
 
 function Profile() {
+  const message = "Hey, I am Elvin";
   return (
     <header className="profile-container">
-      <img src={profile} className="profile-img" alt="hey" />
-      <span className="header-text">Hey, I am Elvin!</span>
+      <img draggable={false} src={profile} className="profile-img" alt="hey" />
+      <span className="header-text">
+        {message.split("").map((letter) => (
+          <span className="letter">{letter}</span>
+        ))}
+      </span>
       <ul className="social-icons">
         {socials.map((social) => {
           return <SocialIcon social={social} key={social.id} />;
