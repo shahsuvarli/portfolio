59d80c7a71fcbc485f282080b3bda12f4e9117d4
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2025-11-06 21:17:25 +0100

    Remvoed vercel json file

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ important for GitHub Pages or custom domain
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
